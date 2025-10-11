aadd6cf75b7a02ad0434a3d33b2efb2a75f81379
peter brandal <peterabrandal@gmail.com>
2025-10-11 22:28:50 +1100

    format

--- src/components/weekplan-card.tsx
+++ src/components/weekplan-card.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link } from 'react-router';
 
 export function WeekplanCard({
   title,
@@ -11,13 +11,13 @@ export function WeekplanCard({
 }) {
   return (
     <Link to="/weekplanner" className="block h-full">
-    <div
-      onClick={onClick}
-      className="cursor-pointer bg-white rounded-2xl shadow-sm border border-gray-200 p-5 hover:shadow-md transition"
-    >
-      <h2 className="text-xl font-semibold text-gray-900 mb-1">{title}</h2>
-      <p className="text-gray-500 text-sm">av {author}</p>
-    </div>
+      <div
+        onClick={onClick}
+        className="cursor-pointer bg-white rounded-2xl shadow-sm border border-gray-200 p-5 hover:shadow-md transition"
+      >
+        <h2 className="text-xl font-semibold text-gray-900 mb-1">{title}</h2>
+        <p className="text-gray-500 text-sm">av {author}</p>
+      </div>
     </Link>
   );
 }
