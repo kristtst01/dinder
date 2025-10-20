588db91508d6899ed9295fd882a96b16d7600629
Kristian Tufte Sterud <kristian0805@hotmail.com>
2025-10-20 21:16:28 +0200

    add claudeignore and remove migrations folder

--- supabase/migrations/20251020182622_add_recipe_image_and_area.sql
+++
@@ -1,7 +0,0 @@
--- Add image and area columns to recipes table
-ALTER TABLE recipes
-ADD COLUMN image TEXT,
-ADD COLUMN area TEXT;
-
--- Add index on area for better filtering performance
-CREATE INDEX idx_recipes_area ON recipes(area);
